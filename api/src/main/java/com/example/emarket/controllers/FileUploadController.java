a5cfb6c69c667c200c46567c5b001f894dc71445
lehananh0606 <lehananh06@gmail.com>
2023-09-19 10:23:46 +0700

    fixed-customer-loading-file

--- api/src/main/java/com/example/emarket/controllers/FileUploadController.java
+++ api/src/main/java/com/example/emarket/controllers/FileUploadController.java
@@ -4,7 +4,6 @@ import com.example.emarket.responses.ResponseObject;
 import com.example.emarket.services.ApartmentService;
 import com.example.emarket.services.ContractService;
 import com.example.emarket.services.CustomerService;
-import lombok.AllArgsConstructor;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,63 +16,65 @@ import java.io.InputStream;
 
 @RestController
 @CrossOrigin(origins = {"http://localhost:5173", "https://lavender-company-fe.vercel.app", "http://localhost:10001", "http://e-market-frontend:80", "http://localhost:80"}, allowCredentials = "true", allowedHeaders = "*")
-@AllArgsConstructor
+
 @RequestMapping("/api/upload")
 public class FileUploadController {
 
-//    private final CustomerService customerService;
-//    private final ApartmentService apartmentService;
-//    private final ContractService contractService;
-//
-//    private final Logger logger = LogManager.getLogger(FileUploadController.class);
-//
-//    @PostMapping
-//    public ResponseEntity<ResponseObject> uploadFiles(MultipartFile[] files) {
-//        logger.info("Start to save data from files");
-//
-//        try {
-//
-//            return ResponseEntity.ok(
-//                    ResponseObject.builder()
-//                            .statusCode(200)
-//                            .message("Saved data from files successfully")
-//                            .data("20 objects saved")
-//                            .build()
-//            );
-//        }catch (Exception e) {
-//            logger.error(e.getMessage());
-//
-//            return ResponseEntity.internalServerError()
-//                    .body(
-//                            ResponseObject.builder()
-//                                    .statusCode(500)
-//                                    .message(e.getMessage())
-//                                    .build()
-//                    );
-//        }
-//    }
-//    @Autowired
-//    public FileUploadController(CustomerService customerService) {
-//        this.customerService = customerService;
-//    }
-//
-//
-//    @PostMapping("/upload-customer")
-//    public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file) {
-//        try {
-//            // Call the service method to process the uploaded file (e.g., save data to the database)
-//            InputStream inputStream = file.getInputStream();
-//            customerService.loadCustomer(inputStream);
-//            return ResponseEntity.status(HttpStatus.OK).body("Data uploaded successfully.");
-//        } catch (Exception e) {
-//            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error uploading and processing the file: " + e.getMessage());
-//        }
-//    }
-//    @PostMapping("/upload-contract")
-//    public ResponseEntity<String> uploadContracts(@RequestParam("files") MultipartFile[] files) {
-//        for (MultipartFile file : files) {
-//            contractService.loadContracts(file);
-//        }
-//        return ResponseEntity.ok("Contracts uploaded successfully");
-//    }
+    private final CustomerService customerService;
+    private final ApartmentService apartmentService;
+    private final ContractService contractService;
+
+    private final Logger logger = LogManager.getLogger(FileUploadController.class);
+
+    @PostMapping
+    public ResponseEntity<ResponseObject> uploadFiles(MultipartFile[] files) {
+        logger.info("Start to save data from files");
+
+        try {
+
+            return ResponseEntity.ok(
+                    ResponseObject.builder()
+                            .statusCode(200)
+                            .message("Saved data from files successfully")
+                            .data("20 objects saved")
+                            .build()
+            );
+        }catch (Exception e) {
+            logger.error(e.getMessage());
+
+            return ResponseEntity.internalServerError()
+                    .body(
+                            ResponseObject.builder()
+                                    .statusCode(500)
+                                    .message(e.getMessage())
+                                    .build()
+                    );
+        }
+    }
+    @Autowired
+    public FileUploadController(CustomerService customerService, ApartmentService apartmentService, ContractService contractService) {
+        this.customerService = customerService;
+        this.apartmentService = apartmentService;
+        this.contractService = contractService;
+    }
+
+
+    @PostMapping("/upload-customer")
+    public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file) {
+        try {
+            // Call the service method to process the uploaded file (e.g., save data to the database)
+            InputStream inputStream = file.getInputStream();
+            customerService.loadCustomer(inputStream);
+            return ResponseEntity.status(HttpStatus.OK).body("Data uploaded successfully.");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error uploading and processing the file: " + e.getMessage());
+        }
+    }
+    @PostMapping("/upload-contract")
+    public ResponseEntity<String> uploadContracts(@RequestParam("files") MultipartFile[] files) {
+        for (MultipartFile file : files) {
+            contractService.loadContracts(file);
+        }
+        return ResponseEntity.ok("Contracts uploaded successfully");
+    }
 }
